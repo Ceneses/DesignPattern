cdc43f84da88b5c312e860c0b2fc5aa60c7545e0
Ceneses <wo51study@163.com>
2020-04-27 10:29:17 +0800

    modify LazySingleton

--- src/Creational/Singleton/LazySingleton.java
+++ src/Creational/Singleton/LazySingleton.java
@@ -1,13 +1,17 @@
 package Creational.Singleton;
 
 public class LazySingleton {
-    private static LazySingleton instance = null;
+    private volatile static LazySingleton instance = null; // 防止指令重排序
     private LazySingleton(){
 
     }
     public static LazySingleton getInstance(){
-        if(instance == null){
-           instance = new LazySingleton();
+        if(instance == null){ // 如果instance已经被赋值，那么就直接赋值
+            synchronized (LazySingleton.class){
+                if(instance == null){ // 当2条线程同时进行到这个地方的时候，还是需要进行null判断
+                    instance = new LazySingleton();
+                }
+            }
         }
         return instance;
     }
